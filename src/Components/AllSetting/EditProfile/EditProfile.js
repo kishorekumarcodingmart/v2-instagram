0d934164ad3ca3fd53c6c9b0170e970b8ebf8c6d
M R Kishore Kumar <kishorekumar@codingmart.com>
2022-10-10 12:10:27 +0530

    Edit Setting Final

--- src/Components/AllSetting/EditProfile/EditProfile.js
+++ src/Components/AllSetting/EditProfile/EditProfile.js
@@ -2,83 +2,84 @@ import React from 'react'
 import './EditProfile.css'
 import Input from '../../Input/Input'
 
-{/* <input 
-          onChange={onChange}
-          type={type} 
-          name={name} 
-          placeholder={placeholder} 
-          className={className} 
-          value={value}
-        /> */}
+
 function EditProfile() {
   return (
     <>
         <section className='EditProfileWrapper flex flex-col justify-content-center'>
-            <table className='flex justify-content-center'>
-                <tbody className='flex flex-col justify-content-center align-items-center '>
-                    <tr className='flex gap-3'>
-                        <td><img src={"https://instagram-clone-mrkishorekumar.netlify.app/img.png"} className="profilePicSetting" alt="dp" /></td>
+            <table>
+                <tbody>
+                    <tr>
+                        <td className='text-right'><img src={"https://instagram-clone-mrkishorekumar.netlify.app/img.png"} className="profilePicSetting" alt="dp" /></td>
                         <td>
-                            <div>mrkishorekumar</div>
-                            <div className='colorBlue fw-bold'>Change profile photo</div>
+                            <div className='fw-bold'>mrkishorekumar</div>
+                            <div className='colorBlue pt-1'>Change profile photo</div>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Name</td>
-                        <td className='flex flex-col  '>
-                            <Input type="text"   placeholder="Name" className="p-2" />
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Name</td>
+                        <td>
+                            <Input type="text" placeholder="Name" className="p-2" />
+                            <div className='help'>Help people discover your account by using the name that you're known by: either your full name, nickname or business name.</div>
+                            <div className='help'>You can only change your name twice within 14 days.</div>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Username</td>
-                        <td className='flex flex-col  '>
-                            <Input type="text" placeholder="Username" className="p-2" />
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>UserName</td>
+                        <td>
+                            <Input type="text" placeholder="UserName" className="p-2" />
+                            <div className='help'>In most cases, you'll be able to change your username back to mrkishorekumar for another 14 days.<div className='colorBlue'>Learn more.</div></div>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Website</td>
-                        <td className='flex flex-col  '>
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Website</td>
+                        <td>
                             <Input type="text" placeholder="Website" className="p-2" />
+                            <div className='help'>Editing your links is only available on mobile. Visit the Instagram app and edit your profile to change the websites in your bio.</div>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Bio</td>
-                        <td className='flex flex-col  '>
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Bio</td>
+                        <td>
                             <Input type="text" placeholder="Bio" className="p-2" />
+                            <div className='help'>0 / 150</div>
+                            <div className='help fw-bold'>Personal information</div>
+                            <div className='help'>Provide your personal information, even if the account is used for a business, pet or something else. This won't be part of your public profile.</div>
                         </td>
                     </tr>
-
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Email address</td>
-                        <td className='flex flex-col  '>
-                            <Input type="text"   placeholder="Email address" className="p-2" />
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Email</td>
+                        <td>
+                            <Input type="text" placeholder="Email address" className="p-2" />
+                            <button className='p-2 mt-2'>Confirm Email Address</button>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Phone number</td>
-                        <td className='flex flex-col'>
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Phone</td>
+                        <td>
                             <Input type="text" placeholder="Phone number" className="p-2" />
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Gender</td>
-                        <td className='flex flex-col'>
+                    <tr>
+                        <td className='fw-bold flex justify-content-end'>Gender</td>
+                        <td>
                             <Input type="text" placeholder="Gender" className="p-2" />
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'>Similar account suggestions</td>
-                        <td className=''>
-                            <Input type="checkbox" placeholder="Bio" className="" />
-                            <p>Include your account when recommending similar accounts that people might want to follow.</p>
+                    <tr>
+                        <td className='fw-bold text-right'>Similar account suggestions</td>
+                        <td>
+                            <Input type="checkbox" placeholder="Gender" className="suggestionsBox p-2" />
+                            <div className='suggestionsBoxtext'>Include your account when recommending similar accounts that people might want to follow.</div>
                         </td>
                     </tr>
-                    <tr className='flex  align-items-center gap-3'>
-                        <td className='fw-bold'><button className='p-2'>Sumbit</button></td>
-                        <td className='colorBlue fw-bold'>Temporarily Deactivate Your Account</td>
+                    <tr>
+                        <td><button className='p-2'>Sumbit</button></td>
+                        <td className='fw-bold colorBlue'>Temporarily deactivate my account</td>
                     </tr>
                 </tbody>
             </table>
+            
         </section>
     </>
   )
