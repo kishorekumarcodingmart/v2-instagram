69497e6ac89dc8a18c81e6ec145f14f2b0d102a5
M R Kishore Kumar <kishorekumar@codingmart.com>
2022-10-11 10:02:21 +0530

    /* eslint-disable */

--- src/Helper/Validate.js
+++ src/Helper/Validate.js
@@ -1,15 +1,19 @@
 export const validateEmail = (email) => {
+    /* eslint-disable */
     return email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 };
 
 export const validatePhone = (phone) => {
+    /* eslint-disable */
     return phone.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/);
 };
 
 export const validateUserName = (userName) => {
+    /* eslint-disable */
     return userName.match(/^[a-z0-9_\.]+$/);
 };
 
 export const validatePassword = (password) => {
+    /* eslint-disable */
     return password.match(/^\w{8,15}$/);
 };
