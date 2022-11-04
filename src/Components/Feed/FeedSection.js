920c6eff6dc4d51d3f2f5f0173590def8f8cbf09
M R Kishore Kumar <kishorekumar@codingmart.com>
2022-11-04 14:51:58 +0530

    srcoll

--- src/Components/Feed/FeedSection.js
+++ src/Components/Feed/FeedSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './FeedSection.css'
 import { Link } from 'react-router-dom'
 import IndividualStory from '../IndividualStory/IndividualStory'
@@ -6,50 +6,8 @@ import IndividualPost from '../IndividualPost/IndividualPost'
 import SuggestionAccount from '../SuggestionAccount/SuggestionAccount'
 import {feedFooeterLinkList} from '../../Utils/Const'
 import StoryModel from '../StoryModel/StoryModel'
+import Post from "../../Utils/Feed.json"
 
-
-const post = [
-    {
-        multiPost : false,
-        profilePhoto : "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVvcGxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-        postUrl : "https://images.unsplash.com/photo-1485206412256-701ccc5b93ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBlb3BsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-        likes : "404",
-        username : "satisfyingson",
-        description : "Never sacrifice three things: family, love, and or yourself.",
-        commentCount : "3",
-        timeStamp : "1 Day Ago"
-    },
-    {
-        multiPost : true,
-        profilePhoto : "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-        postUrl : [{url: "https://images.unsplash.com/photo-1543523195-e0613799d7ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTF8fHByb2ZpbGV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},{url: "https://images.unsplash.com/photo-1520315342629-6ea920342047?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fHByb2ZpbGV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"}],
-        likes : "404",
-        username : "raftingscroll",
-        description : "Never sacrifice three things: family, love, and or yourself.",
-        commentCount : "3",
-        timeStamp : "1 Day Ago"
-    },
-    {
-        multiPost : false,
-        profilePhoto : "https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlb3BsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-        postUrl : "https://images.unsplash.com/photo-1483995564125-85915c11dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBlb3BsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-        likes : "404",
-        username : "magicauthentic",
-        description : "Never sacrifice three things: family, love, and or yourself.",
-        commentCount : "3",
-        timeStamp : "1 Day Ago"
-    },
-    {
-        multiPost : true,
-        profilePhoto : "https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBlb3BsZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-        postUrl : [{url: "https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"},{url: "https://images.unsplash.com/photo-1528271537-64e11fc31bba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"}],
-        likes : "404",
-        username : "writerfascinated",
-        description : "Never sacrifice three things: family, love, and or yourself.",
-        commentCount : "3",
-        timeStamp : "1 Day Ago"
-    },
-]
 
 const story = [
     {
@@ -129,7 +87,28 @@ const suggestion = [
 
 
 function FeedSection() {
-    // console.log(postSection)
+
+    const rows = 5
+
+    const [post, setPost] = useState([...Post.slice(0,5)])
+    const [page, setPage] = useState(1)
+
+    useEffect(()=>{
+        console.log(page*rows-rows)
+        console.log(page*rows)
+        setPost([...Post.slice(0,page*rows)])
+      },[page])
+
+    const handlescroll = () => {
+        if(window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight){
+            setPage(prev=>prev+1)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll',handlescroll)
+      },[])
+
     const [refState , setRefState] = useState("")
 
     const [loading, setLoading ] = useState(true)
